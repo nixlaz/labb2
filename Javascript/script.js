51a8d5e17d87da279f9f0d4773a8f4a2abd935c2
Niclas <niclastornkvist@gmail.com>
2021-09-22 18:07:26 +0200

    commit

--- Javascript/script.js
+++
@@ -1,8 +0,0 @@
-let gurkKnapp = document.getElementById("gurkknapp")
-
-gurkKnapp.onclick = function bytaTextFunktion() {
-  
-    let tomat = document.getElementById('tomat');
-    tomat.innerHTML = "gurka";
-    tomat.style.color = "green";
-};
